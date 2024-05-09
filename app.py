6b305082da972f4b2e32951c63ac349baf4b9222
sarvind1119 <69563525+sarvind1119@users.noreply.github.com>
2024-05-09 10:10:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ region = 'us-east-1'
 
 spec = ServerlessSpec(cloud=cloud, region=region)
 
-index_name = 'arvind1'
+index_name = 'meity5924'
 
 # get openai api key from platform.openai.com
 OPENAI_API_KEY =  os.environ.get('OPENAI_API_KEY')
